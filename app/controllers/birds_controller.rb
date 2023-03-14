224307a6d80bf9c436fe4652bc289a065f0f840a
+ <+>
2023-03-15 01:43:50 +0300

    Code along

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -25,7 +25,27 @@ class BirdsController < ApplicationController
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
+  end
+
+  def update
+    bird = Bird.find_by(id:params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: {error: "Bird not found"}, status: :not_found
+    end
+  end
+
+  def increment_likes
+    bird = Bird.find_by(id:params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
   end
 
 end
